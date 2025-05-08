cbd74e998558ffd8ddc7678716dfc57f033653e6
williams <williamsmatiassan@gmail.com>
2025-05-07 21:48:51 -0600

    ordenacion

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const videoList = [
+    "/recursos/video1.mp4",
+    "/recursos/video.mp4",
+    "/recursos/video2.mp4",
+];
+
+const videoElement = document.querySelector("main video");
+let currentVideoIndex = 0;
+
+videoElement.src = videoList[currentVideoIndex];
+videoElement.play();
+
+videoElement.addEventListener("ended", () => {
+    currentVideoIndex = (currentVideoIndex + 1) % videoList.length;
+    videoElement.src = videoList[currentVideoIndex];
+    videoElement.play();
+});
