e2d28e8735a7049cba2cd932d9a8a3bd76fd3f3a
williams <williamsmatiassan@gmail.com>
2025-05-07 22:07:50 -0600

    nuevos recursos

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 const videoList = [
+    "/recursos/video3.mp4",
+    "/recursos/video4.mp4",
+    "/recursos/video5.mp4",
+    "/recursos/video6.mp4",
     "/recursos/video1.mp4",
     "/recursos/video.mp4",
     "/recursos/video2.mp4",
